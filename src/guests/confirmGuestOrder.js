bb812650b17ad996486ed29333a82b781bdd1598
Sangram Reddy <reddy.horcrux@gmail.com>
2020-02-22 11:02:57 +0000

    refactored deployment

--- src/guests/confirmGuestOrder.js
+++ src/guests/confirmGuestOrder.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
 const mysql = require('serverless-mysql')({
     config: {
         host: process.env.HOST,
@@ -13,6 +16,10 @@ const mysql = require('serverless-mysql')({
 module.exports.fun = async (event, context, callback) => {
     global.fetch = require('node-fetch');
     console.log(event)
+    // Set the region
+    AWS.config.update({region: process.env.REGION});
+    // Create an SQS service object
+    var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
     let data = event.body
     let order_id = Number(event.path.id)
     let query = `
@@ -21,5 +28,27 @@ module.exports.fun = async (event, context, callback) => {
     console.log("Running query", query);
     let results = await mysql.query(query, [ data.session_id, order_id, data.charge_id ])
     await mysql.end()
+    var params = {
+        DelaySeconds: 5,
+        MessageAttributes: {
+            "Author": {
+                DataType: "String",
+                StringValue: data.description
+            },
+            "WeeksOn": {
+                DataType: "Number",
+                StringValue: "7"
+            }
+        },
+        MessageBody: JSON.stringify(results),
+        QueueUrl: process.env.SQS_QUEUE_URL
+    };
+    console.log("Firing message to: ", process.env.SQS_QUEUE_URL);
+    const response = await sqs.sendMessage(params).promise();
+    if(response) {
+        console.log("Message queued to SQS successfully: ", response);
+    } else {
+        console.log("Message queued failed");
+    }
     return results
 }
